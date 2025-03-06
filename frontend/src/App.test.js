6ca693c329f689fcaefecb04486713cb96a2ecc7
priyasalwan <103571646+priyasalwan@users.noreply.github.com>
2025-03-06 04:03:54 +0000

    Fixed test by adding missing imports

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App"; // Import the App component
 
-test('renders learn react link', () => {
+test("renders DevOps To-Do List title", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/DevOps To-Do List/i);
+  expect(titleElement).toBeInTheDocument();
 });
